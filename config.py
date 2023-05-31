2e6b5141a3ece6a1e804dbcf337f63ee28c57cda
X-3306 <75887512+X-3306@users.noreply.github.com>
2023-06-01 01:51:46 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Ustawienia bankomatu
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BACKGROUND_COLOR = (30, 30, 30)  # Kolor tła ekranu bankomatu
+FONT_COLOR = (255, 255, 255)  # Kolor czcionki na ekranie bankomatu
+BUTTON_COLOR = (50, 50, 50)  # Kolor przycisków na ekranie bankomatu
+BUTTON_TEXT_COLOR = (255, 255, 255)  # Kolor tekstu na przyciskach
+BANK_ACCOUNTS_FILE = 'bank_accounts.json'  # Plik przechowujący informacje o kontach bankowych
+TRANSACTION_HISTORY_FILE = 'transaction_history.json'  # Plik przechowujący historię transakcji
+
+# Ustawienia serwera
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 8888
